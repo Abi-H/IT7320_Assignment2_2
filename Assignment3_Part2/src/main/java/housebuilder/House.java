0c4be49f2615c2c39a71133049c5d8a113d83cfd
abi <abiga@Galahad>
2018-10-02 09:58:16 +1300

    initial commit

---
+++ Assignment3_Part2/src/main/java/housebuilder/House.java
@@ -0,0 +1,53 @@
+package housebuilder;
+
+public class House {
+	int pricePerSquareMetre, squareMetres;
+	String houseType;
+
+	public House(int squareMetres, String houseType) {
+		setHouseType(houseType);
+		setSquareMetres(squareMetres);
+		setPricePerSquareMetre(getHouseType());
+	}
+
+	public int getPricePerSquareMetre() {
+		return pricePerSquareMetre;
+	}
+
+	public void setPricePerSquareMetre(String type) {
+		if (type.equals("deluxe")) {
+			this.pricePerSquareMetre = 1000;
+		} else if (type.equals("custom")) {
+			this.pricePerSquareMetre = 1500;
+		} else if (type.equals("deluxe custom")) {
+			this.pricePerSquareMetre = 2500;
+		} else {
+			setHouseType("basic");
+			this.pricePerSquareMetre = 620;
+		}
+	}
+
+	public String getHouseType() {
+		return houseType;
+	}
+
+	public void setHouseType(String houseType) {
+		this.houseType = houseType;
+	}
+
+	public int getSquareMetres() {
+		return squareMetres;
+	}
+
+	public void setSquareMetres(int squareMetres) {
+		this.squareMetres = squareMetres;
+	}
+
+	public boolean withinBudget(int budget) {
+		return getTotalPrice() <= budget;
+	}
+
+	public int getTotalPrice() {
+		return pricePerSquareMetre * squareMetres;
+	}
+}
---
+++ Assignment3_Part2/src/test/java/housebuilder/HouseTest.java
@@ -0,0 +1,72 @@
+package housebuilder;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class HouseTest {
+
+
+	House basicHouse, customHouse, deluxeHouse, deluxeCustomHouse;
+	int budget, basicSize, deluxeSize, customSize, deluxeCustomSize, smallBudget, modestBudget, largeBudget, maxBudget;
+
+	@Before
+	public void setUp() throws Exception {
+		basicSize = 160;
+		deluxeSize = 240;
+		customSize = 320;
+		deluxeCustomSize = 400;
+		smallBudget = 100000;
+		modestBudget = 250000;
+		largeBudget = 500000;
+		maxBudget = 1000000;
+
+		basicHouse = new House(basicSize, "basic");
+		deluxeHouse = new House(deluxeSize, "deluxe");
+		customHouse = new House(customSize, "custom");
+		deluxeCustomHouse = new House(deluxeCustomSize, "deluxe custom");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		basicHouse = null;
+		deluxeHouse = null;
+		customHouse = null;
+		deluxeCustomHouse = null;
+	}
+
+	@Test
+	public void testWithinSmallBudget() {
+		assertTrue(basicHouse.withinBudget(smallBudget));
+		assertFalse(deluxeHouse.withinBudget(smallBudget));
+		assertFalse(customHouse.withinBudget(smallBudget));
+		assertFalse(deluxeCustomHouse.withinBudget(smallBudget));
+	}
+
+	@Test
+	public void testWithinModestBudget() {
+		assertTrue(basicHouse.withinBudget(modestBudget));
+		assertTrue(deluxeHouse.withinBudget(modestBudget));
+		assertFalse(customHouse.withinBudget(modestBudget));
+		assertFalse(deluxeCustomHouse.withinBudget(modestBudget));
+	}
+
+	@Test
+	public void testWithinLargeBudget() {
+		assertTrue(basicHouse.withinBudget(largeBudget));
+		assertTrue(deluxeHouse.withinBudget(largeBudget));
+		assertTrue(customHouse.withinBudget(largeBudget));
+		assertFalse(deluxeCustomHouse.withinBudget(largeBudget));
+	}
+	
+	@Test
+	public void testWithinMaxBudget() {
+		assertTrue(basicHouse.withinBudget(maxBudget));
+		assertTrue(deluxeHouse.withinBudget(maxBudget));
+		assertTrue(customHouse.withinBudget(maxBudget));
+		assertTrue(deluxeCustomHouse.withinBudget(maxBudget));
+	}
+
+}
